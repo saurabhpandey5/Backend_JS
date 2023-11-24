38b8f514c43da83980ca13e27b6d6568dd7dcdd9
saurabh.pandey3 <saurabh.pandey3@gmail.com>
2023-11-24 19:51:37 +0530

    Error code

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,20 +1,21 @@
 // require('dotenv').config({path:'./env'})
-import dotenv from 'dotenv';
-import connectDB from './db/index.js';
-
-
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
-connectDB();
+  path: "./env",
+});
 
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port : 
+      ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed !!! ", err);
+  });
 
 /*
 import express from 'express';
