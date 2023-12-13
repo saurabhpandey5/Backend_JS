983daede6dbdd7ff68cfb704534abfb9d48d20b4
saurabh.pandey3 <saurabh.pandey3@gmail.com>
2023-12-13 11:17:30 +0530

    Router

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -25,4 +25,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+
+// routes import
+import userrouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use('/api/v1/users',userrouter)
+
+//http://localhost:8080/api/v1/users/register
 export { app };
